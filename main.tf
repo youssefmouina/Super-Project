9aed1de07e846d4db5ed65a62ca70e5b42683490
DevopsWorking <159310985+DevopsWorking@users.noreply.github.com>
2024-03-08 00:52:30 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region  # Change to your desired region
+}
+
+module "nexus_instance" {
+  source       = "./ec2_instance"
+  ami          = var.aws_ami  # Example AMI ID, replace with your desired AMI
+  instance_type = var.aws_type             # Example instance type, replace with your desired type
+  instance_name = "nexus"
+}
+
+module "sonar_instance" {
+  source       = "./ec2_instance"
+  ami          = var.aws_ami  # Example AMI ID, replace with your desired AMI
+  instance_type = var.aws_type              # Example instance type, replace with your desired type
+  instance_name = "sonar"
+}
