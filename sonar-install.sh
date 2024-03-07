5b83735341a12cb7f1678374ec29747939c67b5f
DevopsWorking <159310985+DevopsWorking@users.noreply.github.com>
2024-03-08 01:42:28 +0530

    Update sonar-install.sh

--- sonar-install.sh
+++ sonar-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo yum install java-17-amazon-corretto-devel
+sudo yum install java-17-amazon-corretto-devel -y
 sudo useradd sonarqube
 wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-10.1.0.73491.zip
 sudo mv sonarqube-10.1.0.73491.zip /opt/sonarqube-10.1.0.73491.zip
