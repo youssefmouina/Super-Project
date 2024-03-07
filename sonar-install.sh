e41f94f752bd0ae040bca31c885706d0dab1c70e
DevopsWorking <159310985+DevopsWorking@users.noreply.github.com>
2024-03-08 00:10:46 +0530

    Create sonar-install.sh
    This is the script for sonar installation

---
+++ sonar-install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo yum install java-17-amazon-corretto-devel
+sudo useradd sonarqube
+wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-10.1.0.73491.zip
+sudo mv sonarqube-10.1.0.73491.zip /opt/sonarqube-10.1.0.73491.zip
+sudo unzip sonarqube-10.1.0.73491.zip 
+sudo chown -R sonarqube:sonarqube sonarqube-10.1.0.73491
+cd sonarqube-10.1.0.73491/
+cd bin
+cd linux-x86-64/
+./sonar.sh start
