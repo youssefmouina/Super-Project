fb6bf41afc069999beefa03d1dc3ce282f261282
DevopsWorking <159310985+DevopsWorking@users.noreply.github.com>
2024-03-08 00:54:16 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  description = "AWS region"
+  default = "us-east-1"
+}
+
+variable "aws_type" {
+  description = "AWS ec2 type"
+  default = "t3.medium"
+}
+
+variable "aws_ami" {
+  description = "AWS ami"
+  default = "ami-0440d3b780d96b29d"
+}
