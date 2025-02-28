0f5155effd3e5b59f4aed5fd0f54a7118397f31a
youssefmouina <160956922+youssefmouina@users.noreply.github.com>
2025-02-28 21:16:28 +0000

    Update main.tf

--- ec2_instance/main.tf
+++ ec2_instance/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2" {
   ami           = var.ami
   instance_type = var.instance_type
-  key_name      = "yousef.pem" 
+  key_name      = "yousef" 
   tags = {
     Name = var.instance_name
   }
